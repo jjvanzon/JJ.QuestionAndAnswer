73eb58a6f00cc8f2212533b5cadcbde5696c1f2f
jjvanzon <janjoostvanzon@hotmail.com>
2016-11-25 21:52:13 +0100

    Open Source JJ.Framework and JJ Software Architecture: Make unit test succeed or be inconclusive even when database and service connections fail.

--- Presentation/QuestionAndAnswer.Mvc/ViewMapping/QuestionDetailsViewMapping.cs
+++ Presentation/QuestionAndAnswer.Mvc/ViewMapping/QuestionDetailsViewMapping.cs
@@ -1,13 +1,7 @@
 ﻿using JJ.Framework.Presentation.Mvc;
 using JJ.Presentation.QuestionAndAnswer.Mvc.Names;
 using JJ.Presentation.QuestionAndAnswer.Names;
-using JJ.Presentation.QuestionAndAnswer.Presenters;
 using JJ.Presentation.QuestionAndAnswer.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
 
 namespace JJ.Presentation.QuestionAndAnswer.Mvc.ViewMapping
 {
