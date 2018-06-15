635dda17c60617999dbd1c9e2fbc5ac367a7ea28
jjvanzon <janjoostvanzon@hotmail.com>
2018-06-15 22:09:09 +0200

    Demos.ReturnActions: A working variation without view mappings, with return actions managed at the MVC level, with a custom action dispatcher, with error redirects, are also used for login redirects.

--- Presentation/QuestionAndAnswer/ToEntity/EntityViewModelExtensions.cs
+++ Presentation/QuestionAndAnswer/ToEntity/EntityViewModelExtensions.cs
@@ -1,54 +1,67 @@
-﻿using JJ.Data.QuestionAndAnswer;
+﻿using JJ.Business.QuestionAndAnswer.LinkTo;
+using JJ.Data.QuestionAndAnswer;
 using JJ.Data.QuestionAndAnswer.DefaultRepositories.Interfaces;
-using JJ.Business.QuestionAndAnswer.LinkTo;
-using JJ.Presentation.QuestionAndAnswer.ViewModels.Entities;
 using JJ.Framework.Business;
+using JJ.Presentation.QuestionAndAnswer.ViewModels.Entities;
 
 namespace JJ.Presentation.QuestionAndAnswer.ToEntity
 {
-	internal static class EntityViewModelExtensions
-	{
-		public static QuestionCategory ToEntity(this QuestionCategoryViewModel viewModel, IQuestionCategoryRepository questionCategoryRepository, ICategoryRepository categoryRepository, EntityStatusManager entityStatusManager)
-		{
-			QuestionCategory entity = questionCategoryRepository.TryGet(viewModel.QuestionCategoryID);
-			if (entity == null)
-			{
-				entity = questionCategoryRepository.Create();
-				entityStatusManager.SetIsNew(entity);
-			}
+    internal static class EntityViewModelExtensions
+    {
+        public static QuestionCategory ToEntity(
+            this QuestionCategoryViewModel viewModel,
+            IQuestionCategoryRepository questionCategoryRepository,
+            ICategoryRepository categoryRepository,
+            EntityStatusManager entityStatusManager)
+        {
+            QuestionCategory entity = questionCategoryRepository.TryGet(viewModel.QuestionCategoryID);
 
-			Category category = categoryRepository.TryGet(viewModel.Category.ID);
-			entity.LinkTo(category);
+            if (entity == null)
+            {
+                entity = questionCategoryRepository.Create();
+                entityStatusManager.SetIsNew(entity);
+            }
 
-			return entity;
-		}
+            Category category = categoryRepository.TryGet(viewModel.Category.ID);
+            entity.LinkTo(category);
 
-		public static QuestionFlag ToEntity(this QuestionFlagViewModel viewModel, IQuestionFlagRepository questionFlagRepository, IFlagStatusRepository flagStatusRepository)
-		{
-			QuestionFlag questionFlag = questionFlagRepository.TryGet(viewModel.ID);
-			// TODO: Low prio: This is not the TryGet-Insert-Update pattern. It just happens to work for the QuestionEditViewModel.
-			if (questionFlag != null)
-			{
-				questionFlag.FlagStatus = flagStatusRepository.Get(viewModel.Status.ID);
-				questionFlag.Comment = viewModel.Comment;
-			}
+            return entity;
+        }
 
-			return questionFlag;
-		}
+        public static QuestionFlag ToEntity(
+            this QuestionFlagViewModel viewModel,
+            IQuestionFlagRepository questionFlagRepository,
+            IFlagStatusRepository flagStatusRepository)
+        {
+            QuestionFlag questionFlag = questionFlagRepository.TryGet(viewModel.ID);
 
-		public static QuestionLink ToEntity(this QuestionLinkViewModel viewModel, IQuestionLinkRepository repository, EntityStatusManager entityStatusManager)
-		{
-			QuestionLink entity = repository.TryGet(viewModel.ID);
-			if (entity == null)
-			{
-				entity = repository.Create();
-				entityStatusManager.SetIsNew(entity);
-			}
+            // TODO: Low prio: This is not the TryGet-Insert-Update pattern. It just happens to work for the QuestionEditViewModel.
+            if (questionFlag != null)
+            {
+                questionFlag.FlagStatus = flagStatusRepository.Get(viewModel.Status.ID);
+                questionFlag.Comment = viewModel.Comment;
+            }
 
-			entity.Url = viewModel.Url;
-			entity.Description = viewModel.Description;
+            return questionFlag;
+        }
 
-			return entity;
-		}
-	}
+        public static QuestionLink ToEntity(
+            this QuestionLinkViewModel viewModel,
+            IQuestionLinkRepository repository,
+            EntityStatusManager entityStatusManager)
+        {
+            QuestionLink entity = repository.TryGet(viewModel.ID);
+
+            if (entity == null)
+            {
+                entity = repository.Create();
+                entityStatusManager.SetIsNew(entity);
+            }
+
+            entity.Url = viewModel.Url;
+            entity.Description = viewModel.Description;
+
+            return entity;
+        }
+    }
 }
--- Presentation/QuestionAndAnswer/ToEntity/QuestionViewModelExtensions.cs
+++ Presentation/QuestionAndAnswer/ToEntity/QuestionViewModelExtensions.cs
@@ -1,53 +1,57 @@
-﻿using JJ.Presentation.QuestionAndAnswer.ViewModels.Entities;
+﻿using JJ.Business.QuestionAndAnswer.SideEffects;
 using JJ.Data.QuestionAndAnswer;
 using JJ.Data.QuestionAndAnswer.DefaultRepositories.Interfaces;
 using JJ.Framework.Business;
-using JJ.Business.QuestionAndAnswer.SideEffects;
+using JJ.Presentation.QuestionAndAnswer.ViewModels.Entities;
 
 namespace JJ.Presentation.QuestionAndAnswer.ToEntity
 {
-	internal static class QuestionViewModelExtensions
-	{
-		public static Question ToEntity(
-			this QuestionViewModel viewModel, 
-			IQuestionRepository questionRepository, 
-			ISourceRepository sourceRepository,
-			IQuestionTypeRepository questionTypeRepository,
-			EntityStatusManager entityStatusManager)
-		{
-			Question question = questionRepository.TryGet(viewModel.ID);
+    internal static class QuestionViewModelExtensions
+    {
+        public static Question ToEntity(
+            this QuestionViewModel viewModel,
+            IQuestionRepository questionRepository,
+            ISourceRepository sourceRepository,
+            IQuestionTypeRepository questionTypeRepository,
+            EntityStatusManager entityStatusManager)
+        {
+            Question question = questionRepository.TryGet(viewModel.ID);
 
-			if (question == null)
-			{
-				question = questionRepository.Create();
+            if (question == null)
+            {
+                question = questionRepository.Create();
 
-				entityStatusManager.SetIsNew(question);
-			}
+                entityStatusManager.SetIsNew(question);
+            }
 
-			question.Text = viewModel.Text;
-			question.IsActive = viewModel.IsActive;
-			question.Source = sourceRepository.Get(viewModel.Source.ID);
-			question.QuestionType = questionTypeRepository.Get(viewModel.Type.ID);
+            question.Text = viewModel.Text;
+            question.IsActive = viewModel.IsActive;
+            question.Source = sourceRepository.Get(viewModel.Source.ID);
+            question.QuestionType = questionTypeRepository.Get(viewModel.Type.ID);
 
-			return question;
-		}
+            return question;
+        }
 
-		public static Answer ToAnswer(this QuestionViewModel viewModel, IAnswerRepository answerRepository, EntityStatusManager entityStatusManager)
-		{
-			// TODO: Low prio: Maybe it is better to simply use the question entity as the source of the answer, instead of the repository.
-			Answer answer = answerRepository.TryGetByQuestionID(viewModel.ID);
-			if (answer == null)
-			{
-				answer = answerRepository.Create();
+        public static Answer ToAnswer(
+            this QuestionViewModel viewModel,
+            IAnswerRepository answerRepository,
+            EntityStatusManager entityStatusManager)
+        {
+            // TODO: Low prio: Maybe it is better to simply use the question entity as the source of the answer, instead of the repository.
+            Answer answer = answerRepository.TryGetByQuestionID(viewModel.ID);
 
-				entityStatusManager.SetIsNew(answer);
-			}
+            if (answer == null)
+            {
+                answer = answerRepository.Create();
 
-			ISideEffect sideEffect = new Answer_SideEffect_SetDefaults_ForOpenQuestion(answer, entityStatusManager);
-			sideEffect.Execute();
+                entityStatusManager.SetIsNew(answer);
+            }
 
-			answer.Text = viewModel.Answer;
-			return answer;
-		}
-	}
+            ISideEffect sideEffect = new Answer_SideEffect_SetDefaults_ForOpenQuestion(answer, entityStatusManager);
+            sideEffect.Execute();
+
+            answer.Text = viewModel.Answer;
+            return answer;
+        }
+    }
 }
