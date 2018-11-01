053af7d2c9c3b7aa8f4c1b2b999c37ff3ac2cfe6
jjvanzon <janjoostvanzon@hotmail.com>
2018-11-01 19:29:20 +0100

    JJ.Framework.Presentation: Extend documentation about the PagerViewModel. Trivials.

---
+++ Archive/2018-11-01 Backup of Question_GetPageOfIDs.sql
@@ -0,0 +1,10 @@
+﻿select top (@count) x.ID
+from 
+(
+	select 
+		q.ID, 
+		ROW_NUMBER() over (order by q.ID) as RowNumber 
+	from Question q
+) as x
+where x.RowNumber >= @firstRowNumberOneBased
+order by x.RowNumber;
